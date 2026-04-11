351ec83fe3867a053ed3d2999a5dc216c4c513d1
Rohit Sah <rohitsahps309@gmail.com>
2026-04-12 02:26:15 +0530

    project structure and cart

---
+++ frontend_project/src/components/Product/productdata.jsx
@@ -0,0 +1,85 @@
+import headphoneImg from "../../assets/Headphone.jpg";
+import mouseImg from "../../assets/HP_mouse.jpg";
+import monitorImg from "../../assets/asus_monitor.jpg";
+import keyboardImg from "../../assets/keyboard.png";
+import chargerImg from "../../assets/charger.webp";
+import airpodsImg from "../../assets/airpods.jpeg";
+
+const productsdata = [
+    {
+        id : 1,
+        tag : "Best Seller",
+        category: "Audio",
+        brand: "Marshall",
+        name: "Monitor III A.N.C Black",
+        rating:4.4,
+        reviews: 2341,
+        price:2999,
+        image: headphoneImg
+    },
+    {
+        id : 2,
+        tag : "New",
+        category: "Gaming",
+        brand: "DELL",
+        name: "Alienware Wireless Gaming Mouse",
+        rating:4.4,
+        reviews: 1890,
+        price:8199,
+        oldPrice:9999, 
+        image: mouseImg
+    },
+    {
+        id : 3,
+        tag : "Top Rated",
+        category: "Displays",
+        brand: "ASUS",
+        name: "proArt Display PA32UCDM",
+        rating:4.9,
+        reviews: 987,
+        price:201889,
+        oldPrice:300000,
+        image: monitorImg
+    },
+    {
+        id : 4,
+        tag : "",
+        category: "Peripherals",
+        brand: "HP",
+        name: "HP 970 Creator Keyboard (Wireless)",
+        rating:4.2,
+        reviews: 987,
+        price:7999,
+        oldPrice:11989,
+        image: keyboardImg
+    },
+
+    {
+        id : 5,
+        tag : " ",
+        category: "Accessories",
+        brand: "Nothing",
+        name: "Nothing 65W Fast Charger",
+        rating:4.2,
+        reviews: 987,
+        price:2499,
+        oldPrice:3999, 
+        image: chargerImg
+    },
+    {
+        id : 6,
+        tag : "Premium ",
+        category: "Audio",
+        brand:"Apple",
+        name: "AirPods Pro 3",
+        rating:4.4,
+        reviews: 987,
+        price:24900,
+        oldPrice:25900,
+        image: airpodsImg
+    }
+
+
+];
+
+export default productsdata
--- frontend_project/src/pages/products.jsx
+++ frontend_project/src/pages/products.jsx
@@ -1,97 +1,14 @@
 import "./products.css";
 import { useState } from "react";
 import ShoppingCart from "lucide-react/dist/esm/icons/shopping-cart";
-import headphoneImg from "../assets/Headphone.jpg";
-import mouseImg from "../assets/HP_mouse.jpg";
-import monitorImg from "../assets/asus_monitor.jpg";
-import keyboardImg from "../assets/keyboard.png";
-import chargerImg from "../assets/charger.webp";
-import airpodsImg from "../assets/airpods.jpeg";
-import useCart from "/src/components/cart/useCart"
-
-const products = [
-    {
-        id : 1,
-        tag : "Best Seller",
-        category: "Audio",
-        brand: "Marshall",
-        name: "Monitor III A.N.C Black",
-        rating:4.4,
-        reviews: 2341,
-        price:2999,
-        image: headphoneImg
-    },
-    {
-        id : 2,
-        tag : "New",
-        category: "Gaming",
-        brand: "DELL",
-        name: "Alienware Wireless Gaming Mouse",
-        rating:4.4,
-        reviews: 1890,
-        price:8199,
-        oldPrice:9999, 
-        image: mouseImg
-    },
-    {
-        id : 3,
-        tag : "Top Rated",
-        category: "Displays",
-        brand: "ASUS",
-        name: "proArt Display PA32UCDM",
-        rating:4.9,
-        reviews: 987,
-        price:201889,
-        oldPrice:300000,
-        image: monitorImg
-    },
-    {
-        id : 4,
-        tag : "",
-        category: "Peripherals",
-        brand: "HP",
-        name: "HP 970 Creator Keyboard (Wireless)",
-        rating:4.2,
-        reviews: 987,
-        price:7999,
-        oldPrice:11989,
-        image: keyboardImg
-    },
-
-    {
-        id : 5,
-        tag : " ",
-        category: "Accessories",
-        brand: "Nothing",
-        name: "Nothing 65W Fast Charger",
-        rating:4.2,
-        reviews: 987,
-        price:2499,
-        oldPrice:3999, 
-        image: chargerImg
-    },
-    {
-        id : 6,
-        tag : "Premium ",
-        category: "Audio",
-        brand:"Apple",
-        name: "AirPods Pro 3",
-        rating:4.4,
-        reviews: 987,
-        price:24900,
-        oldPrice:25900,
-        image: airpodsImg
-    }
-
-
-];
+import useCart from "../components/cart/useCart"
+import productsdata from "../components/Product/productdata";
 
 
 
 
 function Products() {
-    const data = useCart();
-    console.log("Products:", data);
+
     
     const {addToCart} = useCart();
     const [selectCategory, setSelectCategory] = useState("All");
@@ -100,7 +17,7 @@ function Products() {
 
     const filterProducts = 
     selectCategory === "All"
-    ? products: products.filter((p) => p.category.toLowerCase()  === selectCategory.toLowerCase());
+    ? productsdata: productsdata.filter((p) => p.category.toLowerCase()  === selectCategory.toLowerCase());
     
     
     return(
